2104b0fdc3d52c7a0d3d2712d14baa98999761ff
jmisturini <55112659+jmisturini@users.noreply.github.com>
2024-07-23 19:21:35 -0300

    Correções

--- Adicional Noturno.py
+++ Adicional Noturno.py
@@ -22,23 +22,24 @@ else:
 
 count = 0
 
+
 for worksheet_hours in workbook_hours.worksheets[0:1]:
     os.system('cls')
     print ("\nProcessando Informações da planilha", worksheet_hours.title,"\n")    
-    for registration_cell, add_cell in zip (worksheet_hours['A:A'],worksheet_hours['C:C']):
+    for registration_cell, add_cell, add_min_cell, unity_cell in zip (worksheet_hours['A:A'],worksheet_hours['C:C'],worksheet_hours["E:E"], worksheet_hours["I:I"]): 
         if registration_cell.value is not None:
-            if add_cell.value == "Sim":
-                registration = worksheet_hours.cell(row=registration_cell.row, column=1).value
-                name = worksheet_hours.cell(row=registration_cell.row, column=2).value
-                session = worksheet_hours.cell(row=registration_cell.row, column=4).value
-                time_convert = worksheet_hours.cell(row=registration_cell.row, column=7).value
-                decimal_minutes = ((time_convert * 60) / 100)
-                worksheet_base.cell(row=count+5,column=1).value = registration
-                worksheet_base.cell(row=count+5,column=2).value = name
-                worksheet_base.cell(row=count+5,column=3).value = session
-                worksheet_base.cell(row=count+5,column=7).value = decimal_minutes
-                count += 1
-    
-    workbook_base.save(filepath + "\\Desktop\\" + "Gerador\\Gerados\\Tabela Adicional Noturno Professores" + "(" + str_current_datetime + ")" + ".xlsx")
-
-input("\nPressione qualquer tecla para encerrar a aplicação...")
+            if add_min_cell.value != 0 and add_min_cell.value is not None:
+                if add_cell.value == "Sim":
+                    registration = worksheet_hours.cell(row=registration_cell.row, column=1).value
+                    name = worksheet_hours.cell(row=registration_cell.row, column=2).value
+                    session = worksheet_hours.cell(row=registration_cell.row, column=4).value
+                    time_convert = worksheet_hours.cell(row=registration_cell.row, column=7).value
+                    decimal_minutes = ((time_convert * 60) / 100)
+                    worksheet_base.cell(row=count+5,column=1).value = registration
+                    worksheet_base.cell(row=count+5,column=2).value = name
+                    worksheet_base.cell(row=count+5,column=3).value = session
+                    worksheet_base.cell(row=count+5,column=7).value = decimal_minutes
+                    worksheet_base.cell(row=count+5,column=11).value = unity_cell.value
+                    count += 1
+                    workbook_base.save(filepath + "\\Desktop\\" + "Gerador\\Gerados\\Tabela Adicional Noturno Professores (Unidade " + str(unity_cell.value) + ")" + "(" + str_current_datetime + ")" + ".xlsx")
+#input("\nPressione qualquer tecla para encerrar a aplicação...")
