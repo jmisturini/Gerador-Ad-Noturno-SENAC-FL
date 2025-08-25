348bda48ac5bba44b8ff0c908b51bdddac80c482
jmisturini <joaomisturini@hotmail.com>
2025-08-25 12:21:56 -0300

    Update Filepath and Sorting
    Update the filepath to avoid problems with name folder and the sorting function

--- Adicional Noturno.py
+++ Adicional Noturno.py
@@ -22,16 +22,17 @@ else:
     print ("A pasta já existe")
 
 count=0
+itens_list = 0
 list_addition=[]
-unity_value=2
+list_sorted=[]
 
 for worksheet_hours in workbook_hours.worksheets[0:1]:
     os.system('cls')
     print ("\nProcessando Informações da planilha", worksheet_hours.title,"\n")    
     for registration_cell, add_cell, add_min_cell, unity_cell in zip (worksheet_hours['A:A'],worksheet_hours['C:C'],worksheet_hours["E:E"], worksheet_hours["I:I"]): 
         if registration_cell.value is not None:
-            if add_min_cell.value != 0 and add_min_cell.value is not None:
-                if add_cell.value == "Sim":
+            if add_cell.value == "Sim":
+                if add_min_cell.value !=0 and add_min_cell.value is not None:
                     registration = worksheet_hours.cell(row=registration_cell.row, column=1).value
                     name = worksheet_hours.cell(row=registration_cell.row, column=2).value
                     session = worksheet_hours.cell(row=registration_cell.row, column=4).value
@@ -39,13 +40,16 @@ for worksheet_hours in workbook_hours.worksheets[0:1]:
                     decimal_minutes = ((time_convert * 60) / 100)
                     unity = worksheet_hours.cell(row=registration_cell.row, column=9).value
                     list_addition.append((registration, name, session, decimal_minutes, unity))
-                    list_addition.sort(key=lambda list_addition: list_addition[4])
-                    print (*list_addition, sep='\n')
-                    worksheet_base.cell(row=count+5,column=1).value = list_addition[0+count][0]
-                    worksheet_base.cell(row=count+5,column=2).value = list_addition[0+count][1]
-                    worksheet_base.cell(row=count+5,column=3).value = list_addition[0+count][2]
-                    worksheet_base.cell(row=count+5,column=7).value = list_addition[0+count][3]
-                    worksheet_base.cell(row=count+5,column=11).value = list_addition[0+count][4]
-                    workbook_base.save(filepath + "\\Gerados\\Tabela Adicional Noturno Professores" + str(unity_value) + "(" + str_current_datetime + ")" + ".xlsx")
-                    count += 1
+                    list_sorted=sorted(list_addition, key=lambda list:list[4])
+                    itens_list +=1
+                    print (*list_sorted, sep='\n')
+
+while itens_list > count:
+    worksheet_base.cell(row=count+5,column=1).value = list_sorted[0+count][0]
+    worksheet_base.cell(row=count+5,column=2).value = list_sorted[0+count][1]
+    worksheet_base.cell(row=count+5,column=3).value = list_sorted[0+count][2]
+    worksheet_base.cell(row=count+5,column=7).value = list_sorted[0+count][3]
+    worksheet_base.cell(row=count+5,column=11).value = list_sorted[0+count][4]
+    workbook_base.save(filepath + "\\Gerados\\Tabela Adicional Noturno Professores" + "(" + str_current_datetime + ")" + ".xlsx")
+    count +=1
 input("\nPressione qualquer tecla para encerrar a aplicação...")
